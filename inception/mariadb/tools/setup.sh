041b96f57668174ac8b9b537bd30f3729265750e
pveiga-c <correiapc@hotmail.com>
2024-10-16 17:43:34 +0100

    add worpress and nginx

--- inception/mariadb/tools/setup.sh
+++ inception/mariadb/tools/setup.sh
@@ -1,28 +1,22 @@
 #!/bin/bash
 
-# this script run in the building container
-# it creates start the mariadb service and create the database and users according to the .env file
-# at the end, exec $@ run the next CMD in the Dockerfile.
-# In this case: "mysqld_safe" that restart the mariadb service
-
 # set -ex # print commands & exit on error (debug mode)
 
-DB_NAME=thedatabase
-DB_USER=theuser
-DB_PASSWORD=abc
-DB_PASS_ROOT=123
+DB_NAME=MariaDB_database
+DB_USER=MariaDB_useru
+DB_PASSWORD=qwerty
+DB_PASS_ROOT=123456
 
-# This script starts the MariaDB service.
-# It is intended to be used as part of the setup process for the MariaDB container
-# in the 42-Inception project at 42Lisboa.
+# GRANT ALL PRIVILEGES ON $DB_NAME.* TO 'root'@'%' IDENTIFIED BY '$DB_PASS_ROOT';
+
 service mariadb start
 
 mariadb -v -u root << EOF
 CREATE DATABASE IF NOT EXISTS $DB_NAME;
 CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';
-GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%'
-GRANT ALL PRIVILEGES ON $DB_NAME.* TO 'root'@'%' IDENTIFIED BY '$DB_PASS_ROOT';
+GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%';
 SET PASSWORD FOR 'root'@'localhost' = PASSWORD('$DB_PASS_ROOT');
+
 EOF
 
 sleep 5
